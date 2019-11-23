433d9e95e2bc1a1663f3a5b157b1a47c99e0e68e
Anna Chudyka <anna.chudyka87@gmail.com>
2019-11-23 21:40:16 +0100

    pętla_korekta_1

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,17 +1,25 @@
 public class User {
+    String name;
     int year;
 
-    public User(int year) {
-        this.year = year;
+    public User(String name, int year){
+        this.name=name;
+        this.year=year;
     }
 
-    public int checkSum(int[]years){
+    public int calculateYearsSumAndReturnAverage(int[]years){
         float sum = 0;
         for (int i = 0; i < years.length; i++) {
             sum += years[i];
         }
         sum /= years.length;
         return (int) sum;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getYear(){
+        return this.year;
     }
 }
 
--- src/main/java/UserTest.java
+++ src/main/java/UserTest.java
@@ -1,10 +1,21 @@
 public class UserTest {
     public static void main(String[] args){
 
-        User example=new User();
+        User example=new User(String name, int year);
         int[] years = new int[]{42, 18, 32, 24};
-        int i = example.checkSum(years);
-        example.checkSum(years);
-        System.out.println(example.year);
+        int i = example.calculateYearsSumAndReturnAverage(years);
+        System.out.println(i);
+
+        User userOne=new User("Kasia",42);
+        System.out.println(userOne.getName()+userOne.getYear());
+
+        User userTwo=new User("Krystian",18);
+        System.out.println(userTwo.name+userTwo.year);
+
+        User userThree=new User("Michał",32);
+        System.out.println(userThree.name+userThree.year);
+
+        User userFour=new User("Aneta",24);
+        System.out.println(userFour.name+userFour.year);
     }
 }
