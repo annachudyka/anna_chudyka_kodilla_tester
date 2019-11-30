4a8b6f9c69d423d6e4cdc924c368261dc3d9a654
Anna Chudyka <anna.chudyka87@gmail.com>
2019-11-30 11:21:34 +0100

    Klasy abstrakcyjne_1

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Person.java
@@ -4,12 +4,12 @@ public class Person {
 
     String firstName;
     int age;
-    String job;
+    String position;
 
-    public Person(String firstName, int age, String job){
+    public Person(String firstName, int age, String position){
         this.firstName=firstName;
         this.age=age;
-        this.job=job;
+        this.position=position;
     }
 
     public String getFirstName(){
@@ -19,16 +19,15 @@ public class Person {
     public int getAge(){
         return age;
     }
-    public String getJob(){
-        return getJob();
+    public String getPosition(){
+        return position;
     }
+
 
     public void displaySalaryAndResponsibilities(Job job){
         System.out.println("Salary: "+job.getSalary());
         job.displayResponsibilities();
     }
-
-
 
     public static void main (String[] args){
 
@@ -37,18 +36,50 @@ public class Person {
         Person person3=new Person("Mateusz",27,"Team Leader");
 
         Job officeAssistance1=new OfficeAssistance();
-        Person person=new Person("x",40,"y");
-        person.displaySalaryAndResponsibilities(officeAssistance1);
+        Person personOne=new Person("x",40,"y");
+        personOne.displaySalaryAndResponsibilities(officeAssistance1);
+
+        Job recruiter1=new Recruiter();
+        Person personTwo=new Person("xx", 41,"yy");
+        personTwo.displaySalaryAndResponsibilities(recruiter1);
+
+        Job teamLeader1=new TeamLeader();
+        Person personThree=new Person("xxx", 42, "yyy");
+        personThree.displaySalaryAndResponsibilities(teamLeader1);
 
 
-        OfficeAssistance officeAssistance=new OfficeAssistance();
-        officeAssistance.displayResponsibilities();
+        person1.getFirstName();
+        String person11=person1.getFirstName();
+        System.out.println(person11);
+        person1.getAge();
+        int person12=person1.getAge();
+        System.out.println(person12);
+        person1.getPosition();
+        String person17=person1.getPosition();
+        System.out.println(person17);
+        person1.displaySalaryAndResponsibilities(officeAssistance1);
 
-        Recruiter recruiter=new Recruiter();
-        recruiter.displayResponsibilities();
+        person2.getFirstName();
+        String person13=person2.getFirstName();
+        System.out.println(person13);
+        person2.getAge();
+        int person14=person2.getAge();
+        System.out.println(person14);
+        person2.getPosition();
+        String person18=person2.getPosition();
+        System.out.println(person18);
+        person2.displaySalaryAndResponsibilities(recruiter1);
 
-        TeamLeader teamLeader=new TeamLeader();
-        teamLeader.displayResponsibilities();
+        person3.getFirstName();
+        String person15=person3.getFirstName();
+        System.out.println(person15);
+        person3.getAge();
+        int person16=person3.getAge();
+        System.out.println(person16);
+        person3.getPosition();
+        String person19=person3.getPosition();
+        System.out.println(person19);
+        person3.displaySalaryAndResponsibilities(teamLeader1);
 
     }
 
