7f857bf1ab1d09e36ff8d87c0ca46ede95a34443
Anna Chudyka <anna.chudyka87@gmail.com>
2019-12-20 11:25:20 +0100

    war game1

--- programming/src/main/java/WarGame/Board.java
+++ programming/src/main/java/WarGame/Board.java
@@ -1,7 +1,24 @@
 package WarGame;
 
+import java.sql.SQLOutput;
+
 public class Board {
     public static void main(String[] args) {
+
+        Human human=new Human();
+        Human human1=new Human();
+        Human human2=new Human();
+        System.out.println(human.putCard());
+        System.out.println(human1.putCard());
+        System.out.println(human2.putCard());
+
+        Computer computer=new Computer();
+        Computer computer1=new Computer();
+        Computer computer2=new Computer();
+        System.out.println(computer.putCard());
+        System.out.println(computer1.putCard());
+        System.out.println(computer2.putCard());
+
 
 
 
